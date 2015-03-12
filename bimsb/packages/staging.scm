6872d5b96462801273d0df842d39259aaac06d7d
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2015-03-12 15:28:46 +0100

    Add new staging module
    This module contains packages that are not yet available in GNU Guix
    upstream, but that will eventually be added after a review.

---
+++ bimsb/packages/staging.scm
@@ -0,0 +1,73 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2015 Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
+;;;
+;;; This file is NOT part of GNU Guix, but is supposed to be used with GNU
+;;; Guix and thus has the same license.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (bimsb packages staging)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages compression))
+
+(define-public bwa
+  (package
+    (name "bwa")
+    (version "0.7.12")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://sourceforge/bio-bwa/bwa-"
+                                  version ".tar.bz2"))
+              (sha256
+               (base32
+                "1330dpqncv0px3pbhjzz1gwgg39kkcv2r9qp2xs0sixf8z8wl7bh"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:tests? #f ;no "check" target
+       #:phases
+       (alist-replace
+        'install
+        (lambda* (#:key outputs #:allow-other-keys)
+          (let ((bin (string-append
+                      (assoc-ref outputs "out") "/bin"))
+                (doc (string-append
+                      (assoc-ref outputs "out") "/share/doc/bwa"))
+                (man (string-append
+                      (assoc-ref outputs "out") "/share/man/man1")))
+            (mkdir-p bin)
+            (mkdir-p doc)
+            (mkdir-p man)
+            (copy-file "bwa" (string-append bin "/bwa"))
+            (copy-file "README.md" (string-append doc "/README.md"))
+            (copy-file "bwa.1" (string-append man "/bwa.1"))))
+        ;; no "configure" script
+        (alist-delete 'configure %standard-phases))))
+    (inputs `(("zlib" ,zlib)))
+    (home-page "http://bio-bwa.sourceforge.net/")
+    (synopsis "Burrows-Wheeler aligner")
+    (description
+     "BWA is a software package for mapping low-divergent sequences against a
+large reference genome, such as the human genome.  It consists of three
+algorithms: BWA-backtrack, BWA-SW and BWA-MEM.  The first algorithm is
+designed for Illumina sequence reads up to 100bp, while the rest two for
+longer sequences ranged from 70bp to 1Mbp.  BWA-MEM and BWA-SW share similar
+features such as long-read support and split alignment, but BWA-MEM, which is
+the latest, is generally recommended for high-quality queries as it is faster
+and more accurate.  BWA-MEM also has better performance than BWA-backtrack for
+70-100bp Illumina reads.")
+    (license license:gpl3)))
