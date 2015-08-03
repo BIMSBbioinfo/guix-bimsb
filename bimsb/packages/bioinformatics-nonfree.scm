15282d20d0a90370b34606245e8e2ad858587b41
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2015-08-03 11:01:52 +0200

    bimsb: Add kallisto.
    * bimsb/packages/bioinformatics-nonfree.scm (kallisto): New variable.

--- bimsb/packages/bioinformatics-nonfree.scm
+++ bimsb/packages/bioinformatics-nonfree.scm
@@ -33,6 +33,7 @@
   #:use-module (gnu packages boost)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages gcc)
+  #:use-module (gnu packages maths)
   #:use-module (gnu packages ncurses)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages python)
@@ -143,6 +144,46 @@ on the difference of read distributions.  It also empirically estimates the
 false discovery rate as a cutoff when two samples have different sequencing
 depths and differentiate reliable RDNPs from the background noise.")
     (license nonfree:artistic1.0)))
+
+(define-public kallisto
+  (package
+    (name "kallisto")
+    (version "0.42.2.1")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/pachterlab/kallisto/archive/v"
+                    version ".tar.gz"))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1yx126g2yrk5gaw42bcd1cpil1c9xllgbkq14dds9y62zvxibygn"))))
+    (build-system cmake-build-system)
+    (arguments `(#:tests? #f)) ; no "check" target
+    (inputs
+     `(("hdf5" ,hdf5)
+       ("zlib" ,zlib)))
+    (home-page "http://pachterlab.github.io/kallisto/")
+    (synopsis "Near-optimal RNA-Seq quantification")
+    (description
+     "kallisto is a program for quantifying abundances of transcripts
+from RNA-Seq data, or more generally of target sequences using
+high-throughput sequencing reads.  It is based on the novel idea of
+pseudoalignment for rapidly determining the compatibility of reads
+with targets, without the need for alignment.  On benchmarks with
+standard RNA-Seq data, kallisto can quantify 30 million human reads in
+less than 3 minutes on a Mac desktop computer using only the read
+sequences and a transcriptome index that itself takes less than 10
+minutes to build.  Pseudoalignment of reads preserves the key
+information needed for quantification, and kallisto is therefore not
+only fast, but also as accurate than existing quantification tools.
+In fact, because the pseudoalignment procedure is robust to errors in
+the reads, in many benchmarks kallisto significantly outperforms
+existing tools.")
+    (license (nonfree:non-free
+              "http://pachterlab.github.io/kallisto/license.html"
+              "This software may only be used for educational and
+research not-for-profit purposes."))))
 
 (define-public macs-1
   (package (inherit macs)
