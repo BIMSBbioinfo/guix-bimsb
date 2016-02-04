bb31e10107ca8685868328da7ab57bffd76a4ab2
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2016-02-04 12:43:59 +0100

    Add bioinformatics-variants module.
    This is for particular variants of any of the packages in Guix
    upstream.
    
    * bimsb/packages/bioinformatics-variants.scm: New file.

---
+++ bimsb/packages/bioinformatics-variants.scm
@@ -0,0 +1,39 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2016 Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
+;;;
+;;; This file is NOT part of GNU Guix, but is supposed to be used with GNU
+;;; Guix and thus has the same license.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (bimsb packages bioinformatics-variants)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix utils)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system python)
+  #:use-module (gnu packages bioinformatics))
+
+;; A different version of MACS2 for Rebecca
+(define-public macs/rebecca
+  (package (inherit macs)
+    (version "2.1.0.20150420.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "MACS2" version))
+       (sha256
+        (base32
+         "1zpxn9nxmfwj8zr8b4gjrx8j81mijgjmagl4prpkghphfbziwraw"))))))
