eb8d18ac919f8057323f1fe5040bd0469238932c
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2016-05-30 20:53:16 +0200

    bimsb: Add rstudio.
    * bimsb/packages/staging.scm (rstudio): New variable.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -24,11 +24,21 @@
   #:use-module (guix git-download)
   #:use-module (guix utils)
   #:use-module (guix build-system ant)
+  #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages)
+  #:use-module (gnu packages boost)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages bioinformatics)
-  #:use-module (gnu packages perl))
+  #:use-module (gnu packages haskell)
+  #:use-module (gnu packages java)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages llvm)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages statistics)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages zip))
 
 ;; This package cannot yet be added to Guix because it bundles an as
 ;; yet unpackaged third-party library, namely "commons-cli-1.1.jar"
@@ -74,3 +84,133 @@
 density estimation allowing identification of biologically meaningful sites
 whose output can be displayed directly in the UCSC Genome Browser.")
       (license license:gpl3+))))
+
+(define-public rstudio
+  (package
+    (name "rstudio")
+    (version "0.99.1201")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/rstudio/rstudio/archive/v"
+                    version ".tar.gz"))
+              (sha256
+               (base32
+                "0b3nipdmxrfq5fhwyy0x9hhf54yqmvdkniyj6x3kmhcha0c33l8n"))
+              (file-name (string-append name "-" version ".tar.gz"))))
+    (build-system cmake-build-system)
+    (arguments
+     `(#:configure-flags '("-DRSTUDIO_TARGET=Server")
+       #:tests? #f ; no tests
+       #:phases
+       (modify-phases %standard-phases
+         (add-before 'build 'set-java-home
+          (lambda* (#:key inputs #:allow-other-keys)
+            (setenv "JAVA_HOME" (assoc-ref inputs "jdk"))
+            #t))
+         (add-after 'unpack 'fix-dependencies
+           (lambda _
+             ;; Disable checks for bundled dependencies.  We take care of them by other means.
+             (substitute* "src/cpp/session/CMakeLists.txt"
+               (("if\\(NOT EXISTS \"\\$\\{RSTUDIO_DEPENDENCIES_DIR\\}/common/rmarkdown\"\\)") "if (FALSE)"))
+             #t))
+         (add-after 'unpack 'copy-clang
+           (lambda* (#:key inputs #:allow-other-keys)
+             (with-directory-excursion "dependencies/common"
+               (let ((clang   (assoc-ref inputs "clang"))
+                     (dir     "libclang")
+                     (lib     "libclang/3.5")
+                     (headers "libclang/builtin-headers"))
+                 (mkdir-p dir)
+                 (mkdir-p lib)
+                 (mkdir-p headers)
+                 (for-each (lambda (file)
+                             (install-file file lib))
+                           (find-files (string-append clang "/lib") ".*"))
+                 (install-file (string-append clang "/include") dir)
+                 #t))))
+         (add-after 'unpack 'unpack-dictionaries
+           (lambda* (#:key inputs #:allow-other-keys)
+             (with-directory-excursion "dependencies/common"
+               (mkdir "dictionaries")
+               (mkdir "pandoc") ; TODO: only to appease the cmake stuff
+               (zero? (system* "unzip" "-qd" "dictionaries"
+                               (assoc-ref inputs "dictionaries"))))))
+         (add-after 'unpack 'unpack-mathjax
+           (lambda* (#:key inputs #:allow-other-keys)
+             (with-directory-excursion "dependencies/common"
+               (mkdir "mathjax-23")
+               (zero? (system* "unzip" "-qd" "mathjax-23"
+                               (assoc-ref inputs "mathjax"))))))
+         (add-after 'unpack 'unpack-gin
+           (lambda* (#:key inputs #:allow-other-keys)
+             (with-directory-excursion "src/gwt"
+               (install-file (assoc-ref inputs "junit") "lib")
+               (mkdir-p "lib/gin/1.5")
+               (zero? (system* "unzip" "-qd" "lib/gin/1.5"
+                               (assoc-ref inputs "gin"))))))
+         (add-after 'unpack 'unpack-gwt
+           (lambda* (#:key inputs #:allow-other-keys)
+             (with-directory-excursion "src/gwt"
+               (mkdir-p "lib/gwt")
+	       (system* "unzip" "-qd" "lib/gwt"
+			(assoc-ref inputs "gwt"))
+               (rename-file "lib/gwt/gwt-2.7.0" "lib/gwt/2.7.0"))
+	     #t)))))
+    (native-inputs
+     `(("pkg-config" ,pkg-config)
+       ("unzip" ,unzip)
+       ("ant" ,ant)
+       ("jdk" ,icedtea "jdk")
+       ("gin"
+        ,(origin
+           (method url-fetch)
+           (uri "https://s3.amazonaws.com/rstudio-buildtools/gin-1.5.zip")
+           (sha256
+            (base32 "155bjrgkf046b8ln6a55x06ryvm8agnnl7l8bkwwzqazbpmz8qgm"))))
+       ("gwt"
+        ,(origin
+           (method url-fetch)
+           (uri "https://s3.amazonaws.com/rstudio-buildtools/gwt-2.7.0.zip")
+           (sha256
+            (base32 "1cs78z9a1jg698j2n35wsy07cy4fxcia9gi00x0r0qc3fcdhcrda"))))
+       ("junit"
+        ,(origin
+           (method url-fetch)
+           (uri "https://s3.amazonaws.com/rstudio-buildtools/junit-4.9b3.jar")
+           (sha256
+            (base32 "0l850yfbq0cgycp8n0r0a1b7xznd37pgfd656vzdwim4blznqmnw"))))
+       ("mathjax"
+        ,(origin
+           (method url-fetch)
+           (uri "https://s3.amazonaws.com/rstudio-buildtools/mathjax-23.zip")
+           (sha256
+            (base32 "16fnq4jsifbldjcvrri3g6d7zhbh22k3jas2jpigmmphnmgd6hjj"))))
+       ("dictionaries"
+        ,(origin
+           (method url-fetch)
+           (uri "https://s3.amazonaws.com/rstudio-dictionaries/core-dictionaries.zip")
+           (sha256
+            (base32 "153lg3ai97qzbqp6zjg10dh3sfvz80v42cjw45zwz7gv1risjha3"))))))
+    (inputs
+     `(("r" ,r)
+       ("r-rmarkdown" ,r-rmarkdown) ; TODO: must be linked to another location
+       ("clang" ,clang-3.5)
+       ("boost" ,boost)
+       ("libuuid" ,util-linux)
+       ("pandoc" ,ghc-pandoc)
+       ("openssl" ,openssl)
+       ("pam" ,linux-pam)
+       ("zlib" ,zlib)))
+    (home-page "http://www.rstudio.org/")
+    (synopsis "Integrated development environment (IDE) for R")
+    (description
+     "RStudio is an integrated development environment (IDE) for the R
+programming language. Some of its features include: Customizable workbench
+with all of the tools required to work with R in one place (console, source,
+plots, workspace, help, history, etc.); syntax highlighting editor with code
+completion; execute code directly from the source editor (line, selection, or
+file); full support for authoring Sweave and TeX documents.  RStudio can also
+be run as a server, enabling multiple users to access the RStudio IDE using a
+web browser.")
+    (license license:agpl3+)))
