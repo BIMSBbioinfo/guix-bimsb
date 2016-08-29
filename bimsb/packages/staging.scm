f80c1a9b0d32a77d79a58bef0455c9125c33179e
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2016-08-29 16:00:52 +0200

    bimsb: Add r-rjson.
    * bimsb/packages/staging.scm (r-rjson): New variable.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -337,6 +337,25 @@ primary sequence.")
 output R objects in HTML format.")
     (license license:lgpl2.1+)))
 
+(define-public r-rjson
+  (package
+    (name "r-rjson")
+    (version "0.2.15")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "rjson" version))
+       (sha256
+        (base32
+         "1vzjyvf57k1fjizlk28rby65y5lsww5qnfvgnhln74qwda7hvl3p"))))
+    (build-system r-build-system)
+    (home-page "http://cran.r-project.org/web/packages/rjson")
+    (synopsis "JSON library for R")
+    (description
+     "This package provides functions to convert R objects into JSON
+objects and vice-versa.")
+    (license license:gpl2+)))
+
 (define-public r-fastcluster
   (package
     (name "r-fastcluster")
