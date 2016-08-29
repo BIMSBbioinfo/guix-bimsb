bbf230863bce57781f98adb7cbeb3aebb838cce9
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2016-08-29 18:04:53 +0200

    bimsb: Add r-sendmailr.
    * bimsb/packages/staging.scm (r-sendmailr): New variable.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -582,6 +582,30 @@ matrices and operations on them using LAPACK and SuiteSparse.")
 more control over dimensions and appearance.")
     (license license:gpl2+)))
 
+(define-public r-sendmailr
+  (package
+    (name "r-sendmailr")
+    (version "1.2-1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "sendmailR" version))
+       (sha256
+        (base32
+         "0z7ipywnzgkhfvl4zb2fjwl1xq7b5wib296vn9c9qgbndj6b1zh4"))))
+    (properties `((upstream-name . "sendmailR")))
+    (build-system r-build-system)
+    (propagated-inputs
+     `(("r-base64enc" ,r-base64enc)))
+    (home-page
+     "http://cran.r-project.org/web/packages/sendmailR")
+    (synopsis "send email using R")
+    (description
+     "This package contains a simple SMTP client which provides a
+portable solution for sending email, including attachment, from within
+R.")
+    (license license:gpl2+)))
+
 (define-public r-fastcluster
   (package
     (name "r-fastcluster")
