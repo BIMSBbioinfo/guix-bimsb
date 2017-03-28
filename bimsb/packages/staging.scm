d18e8d46ce06c0c96fd99c1b2a1804c679dae896
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2017-03-28 14:09:48 +0200

    staging: python-fastcluster: Disable tests.
    * bimsb/packages/staging.scm (python-fastcluster)[arguments]: Disable
    tests.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -1598,7 +1598,8 @@ visualisation, and resampling-based inference.")
     (name "python-fastcluster")
     (build-system python-build-system)
     (arguments
-     `(#:phases
+     `(#:tests? #f
+       #:phases
        (modify-phases %standard-phases
          (add-after 'unpack 'chdir
            (lambda _ (chdir "src/python") #t)))))
