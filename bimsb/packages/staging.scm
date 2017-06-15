ffce319cff49656ad41ee2138ce8b31c069bd5ce
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2017-06-15 15:44:08 +0200

    staging: Hide "which" from (gnu packages base).

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -34,7 +34,7 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages algebra)
   #:use-module (gnu packages autotools)
-  #:use-module (gnu packages base)
+  #:use-module ((gnu packages base) #:hide (which))
   #:use-module (gnu packages boost)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
