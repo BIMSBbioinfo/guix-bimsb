e5e8aedc17441a8df9eba6462d10ab253fb6b5a3
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2017-07-11 11:50:08 +0200

    staging: Add perl-crypt-random-source.
    * bimsb/packages/staging.scm (perl-crypt-random-source): New variable.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -4433,3 +4433,33 @@ previously distributed as part of @code{Type::Tiny} but has since been
 spun off), and can be used with Moose, Mouse and Moo (or none of the
 above).")
     (license (package-license perl))))
+
+(define-public perl-crypt-random-source
+  (package
+    (name "perl-crypt-random-source")
+    (version "0.12")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://cpan/authors/id/E/ET/ETHER/"
+                           "Crypt-Random-Source-" version ".tar.gz"))
+       (sha256
+        (base32
+         "00mw5m52sbz9nqp3f6axyrgcrihqxn7k8gv0vi1kvm1j1nc9g29h"))))
+    (build-system perl-build-system)
+    (native-inputs
+     `(("perl-module-build-tiny" ,perl-module-build-tiny)
+       ("perl-test-exception" ,perl-test-exception)))
+    (propagated-inputs
+     `(("perl-capture-tiny" ,perl-capture-tiny)
+       ("perl-module-find" ,perl-module-find)
+       ("perl-module-runtime" ,perl-module-runtime)
+       ("perl-moo" ,perl-moo)
+       ("perl-namespace-clean" ,perl-namespace-clean)
+       ("perl-sub-exporter" ,perl-sub-exporter)
+       ("perl-type-tiny" ,perl-type-tiny)))
+    (home-page "http://search.cpan.org/dist/Crypt-Random-Source")
+    (synopsis "Get weak or strong random data from pluggable sources")
+    (description "This module provides implementations for a number of
+byte oriented sources of random data.")
+    (license (package-license perl))))
