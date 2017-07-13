25f7331fdab3b5a18108e3e50a86d4ccb5a37fce
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2017-07-13 13:45:28 +0200

    variants: Add missing module.

--- bimsb/packages/bioinformatics-variants.scm
+++ bimsb/packages/bioinformatics-variants.scm
@@ -21,6 +21,7 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix utils)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system python)
