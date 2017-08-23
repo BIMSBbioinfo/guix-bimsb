a4ada7ee67b17154c6b84148d8ac60500098a259
Ricardo Wurmus <rekado@elephly.net>
2017-08-23 17:17:35 +0200

    staging: Remove python-igraph.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -4144,26 +4144,6 @@ technology.  Its features include:
     ;; license text is included.
     (license license:bsd-3)))
 
-(define-public python-igraph
-  (package (inherit igraph)
-    (name "python-igraph")
-    (version "0.7.1.post6")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "python-igraph" version))
-       (sha256
-        (base32
-         "0xp61zz710qlzhmzbfr65d5flvsi8zf2xy78s6rsszh719wl5sm5"))))
-    (build-system python-build-system)
-    (arguments '())
-    (inputs
-     `(("igraph" ,igraph)))
-    (native-inputs
-     `(("pkg-config" ,pkg-config)))
-    (home-page "http://pypi.python.org/pypi/python-igraph")
-    (synopsis "Python bindings for the igraph network analysis library")))
-
 (define-public python-louvain-igraph
   (package
     (name "python-louvain-igraph")
