ff3d2c058276b4bb7d7d84a171f4b43d1d807307
Ricardo Wurmus <rekado@elephly.net>
2017-11-22 10:36:06 +0100

    staging: Add missing module.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -68,6 +68,7 @@
   #:use-module (gnu packages popt)
   #:use-module (gnu packages protobuf)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages python-check)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages readline)
   #:use-module (gnu packages shells)
