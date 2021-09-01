ff2c675045cc25c640eb56434b3f1d488a682708
Ricardo Wurmus <rekado@elephly.net>
2021-09-01 14:55:16 +0200

    staging: r-catch: Move to Guix proper.
    * bimsb/packages/staging.scm (r-catch): Remove variable.

--- bimsb/packages/staging.scm
+++ bimsb/packages/staging.scm
@@ -1029,29 +1029,6 @@ various other algorithms.")
 efficiently analyze genomic data resulting from various experiments.")
     (license license:gpl2)))
 
-(define-public r-catch
-  (let ((commit "60c995e34a6b709832c96bc82128218ecfebd108")
-        (release "1.0")
-        (revision "1"))
-    (package
-      (name "r-catch")
-      (version (string-append release "-" revision "." (string-take commit 9)))
-      (source (origin
-                (method url-fetch)
-                (uri (string-append "https://raw.githubusercontent.com/zhanyinx/CaTCH_R/"
-                                    commit "/CaTCH_" release ".tar.gz"))
-                (sha256
-                 (base32
-                  "052s1ysyvg307q85h4v9flcvfilxw6wpgll417gp5jsygh3kgcpm"))))
-      (build-system r-build-system)
-      (home-page "https://github.com/zhanyinx/CaTCH_R")
-      (synopsis "Call a hierarchy of domains based on Hi-C data")
-      (description "This package allows building the hierarchy of
-domains starting from Hi-C data.  Each hierarchical level is
-identified by a minimum value of physical insulation between
-neighboring domains.")
-      (license license:gpl2))))
-
 (define-public r-shaman
   (let ((commit "52d5edefc1709aa03cd282e00cb703ffd98ea3eb")
         (release "2.0")
